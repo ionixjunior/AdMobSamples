c2507449f09a0924cf186caa631df3d4078cac4d
Ione Souza Junior <junior@ionixjunior.com.br>
2021-04-01 23:50:24 -0300

    Inicializar AdMob

--- iOSSwift/iOSSwift/AppDelegate.swift
+++ iOSSwift/iOSSwift/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import GoogleMobileAds
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -6,7 +7,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        
         return true
     }
 
