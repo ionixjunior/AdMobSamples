ec888e6d982e03d5ae47bc35ddf007f33c53b9a8
Ione Souza Junior <junior@ionixjunior.com.br>
2021-04-02 01:33:48 -0300

    Adicionar botão para abrir a activity do banner na tela inicial

--- AndroidKotlin/app/src/main/java/dev/ionixjunior/androidkotlin/MainActivity.kt
+++ AndroidKotlin/app/src/main/java/dev/ionixjunior/androidkotlin/MainActivity.kt
@@ -2,9 +2,6 @@ package dev.ionixjunior.androidkotlin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdSize
-import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +11,5 @@ class MainActivity : AppCompatActivity() {
 
         MobileAds.initialize(this) { }
 
-        var adBanner = findViewById<AdView>(R.id.adBanner)
-        val adRequest = AdRequest.Builder().build()
-        adBanner.loadAd(adRequest)
     }
 }
