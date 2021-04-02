8ea7ac5200308c03668c784a170e494056de638b
Ione Souza Junior <junior@ionixjunior.com.br>
2021-04-02 01:12:22 -0300

    Inicializar AdMob

--- AndroidKotlin/app/src/main/java/dev/ionixjunior/androidkotlin/MainActivity.kt
+++ AndroidKotlin/app/src/main/java/dev/ionixjunior/androidkotlin/MainActivity.kt
@@ -1,11 +1,14 @@
 package dev.ionixjunior.androidkotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) { }
     }
 }
