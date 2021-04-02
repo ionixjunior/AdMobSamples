cce9c15be1a6bb82f570d00b183639ba4d6e9ba9
Ione Souza Junior <junior@ionixjunior.com.br>
2021-04-02 01:27:14 -0300

    Adicionar activity do banner

---
+++ AndroidKotlin/app/src/main/java/dev/ionixjunior/androidkotlin/BannerActivity.kt
@@ -0,0 +1,11 @@
+package dev.ionixjunior.androidkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BannerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_banner)
+    }
+}
