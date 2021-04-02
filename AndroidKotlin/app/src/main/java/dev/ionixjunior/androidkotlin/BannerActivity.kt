6ae58d61dabe51044ee16451692f476c3e0ae028
Ione Souza Junior <junior@ionixjunior.com.br>
2021-04-02 01:28:10 -0300

    Mover implementação do banner para a activity do banner

--- AndroidKotlin/app/src/main/java/dev/ionixjunior/androidkotlin/BannerActivity.kt
+++ AndroidKotlin/app/src/main/java/dev/ionixjunior/androidkotlin/BannerActivity.kt
@@ -2,10 +2,16 @@ package dev.ionixjunior.androidkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 
 class BannerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_banner)
+
+        var adBanner = findViewById<AdView>(R.id.adBanner)
+        val adRequest = AdRequest.Builder().build()
+        adBanner.loadAd(adRequest)
     }
 }
