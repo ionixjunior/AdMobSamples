c4ffadcc5d9a94e8725f9c4b2fdaad9d212dcacb
Ione Souza Junior <junior@ionixjunior.com.br>
2021-04-02 00:12:20 -0300

    Adicionar banner na view

--- iOSSwift/iOSSwift/ViewController.swift
+++ iOSSwift/iOSSwift/ViewController.swift
@@ -1,7 +1,9 @@
 import UIKit
+import GoogleMobileAds
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var adBanner: GADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
