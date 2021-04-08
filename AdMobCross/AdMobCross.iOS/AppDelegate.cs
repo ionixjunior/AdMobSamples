8d0e970833ef19e89b2d8bf9407e5826bbfd33db
Ione Souza Junior <junior@ionixjunior.com.br>
2021-04-08 20:54:19 -0300

    Referenciar Firebase.Core na inicialização do app iOS
    Isso foi necessário pois a biblioteca do AdMob precisa de algumas funcionalidades do Firebase.Core. No entanto, por não ser referenciado, o linker remove o AppMeasurement e faz a aplicação não compilar.
    
    Este workaround foi encontrado em https://github.com/xamarin/GoogleApisForiOSComponents/issues/397#issuecomment-621561187.

--- AdMobCross/AdMobCross.iOS/AppDelegate.cs
+++ AdMobCross/AdMobCross.iOS/AppDelegate.cs
@@ -23,6 +23,7 @@ namespace AdMobCross.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            _ = Firebase.Core.Configuration.SharedInstance;
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
