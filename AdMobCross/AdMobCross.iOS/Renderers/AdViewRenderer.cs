41972a6de727e54a8aacde320cb79e02849d85a1
Ione Souza Junior <junior@ionixjunior.com.br>
2021-04-08 22:33:28 -0300

    Implementar renderer para exibir o anúncio do AdMob no iOS

---
+++ AdMobCross/AdMobCross.iOS/Renderers/AdViewRenderer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using AdMobCross.Controls;
+using AdMobCross.iOS.Renderers;
+using Google.MobileAds;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer(typeof(AdView), typeof(AdViewRenderer))]
+namespace AdMobCross.iOS.Renderers
+{
+    public class AdViewRenderer : ViewRenderer<AdView, UIView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<AdView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                var window = UIApplication.SharedApplication.KeyWindow;
+                var vc = window.RootViewController;
+                while (vc.PresentedViewController != null)
+                {
+                    vc = vc.PresentedViewController;
+                }
+
+                var adSize = AdSizeCons.Banner;
+                var adBanner = new BannerView(adSize);
+                adBanner.TranslatesAutoresizingMaskIntoConstraints = false;
+                adBanner.AdUnitId = "ca-app-pub-3940256099942544/2934735716";
+                adBanner.RootViewController = vc;
+                adBanner.LoadRequest(Request.GetDefaultRequest());
+                // TODO considerar avaliar a falha do carregamento para alterar a constraint da altura
+
+                e.NewElement.WidthRequest = adSize.Size.Width;
+                e.NewElement.HeightRequest = adSize.Size.Height;
+
+                SetNativeControl(adBanner);
+            }
+        }
+    }
+}
