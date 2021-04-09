c2119774da97c4cddd5afe255eedadf78f2e4388
Ione Souza Junior <junior@ionixjunior.com.br>
2021-04-08 23:09:35 -0300

    Criar renderer para exibir AdMob no Android

---
+++ AdMobCross/AdMobCross.Android/Renderers/AdViewRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using AdMobCross.Controls;
+using AdMobCross.Droid.Renderers;
+using Android.Content;
+using Android.Gms.Ads;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+[assembly: ExportRenderer(typeof(AdMobCross.Controls.AdView), typeof(AdViewRenderer))]
+namespace AdMobCross.Droid.Renderers
+{
+    public class AdViewRenderer : ViewRenderer<Controls.AdView, Android.Views.View>
+    {
+        public AdViewRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(Xamarin.Forms.Platform.Android.ElementChangedEventArgs<Controls.AdView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                var adSize = AdSize.Banner;
+                var adBanner = new Android.Gms.Ads.AdView(Context);
+                adBanner.AdSize = adSize;
+                adBanner.AdUnitId = "ca-app-pub-3940256099942544/6300978111";
+                adBanner.LoadAd(new AdRequest.Builder().Build());
+                // TODO considerar avaliar a falha do carregamento para alterar a constraint da altura
+
+                e.NewElement.WidthRequest = adSize.Width;
+                e.NewElement.HeightRequest = adSize.Height;
+
+                SetNativeControl(adBanner);
+            }
+        }
+    }
+}
