ba0de9fee3834158ddeed3fc12e9e2325f8e0eef
Ione Souza Junior <junior@ionixjunior.com.br>
2021-04-08 22:49:17 -0300

    Inicializar AdMob no Android

--- AdMobCross/AdMobCross.Android/MainActivity.cs
+++ AdMobCross/AdMobCross.Android/MainActivity.cs
@@ -16,6 +16,8 @@ namespace AdMobCross.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Android.Gms.Ads.MobileAds.Initialize(this);
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
