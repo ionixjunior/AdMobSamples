320e90a521df6d2b45e05df37fc29972c441cc8f
Ione Souza Junior <junior@ionixjunior.com.br>
2021-04-15 20:19:14 -0300

    Implementar bindable property para parametrizar o id do anúncio

--- AdMobCross/AdMobCross/Controls/AdView.cs
+++ AdMobCross/AdMobCross/Controls/AdView.cs
@@ -5,5 +5,17 @@ namespace AdMobCross.Controls
 {
     public class AdView : ContentView
     {
+        public static readonly BindableProperty AdIdProperty = BindableProperty.Create(
+            nameof(AdId),
+            typeof(string),
+            typeof(AdView),
+            null,
+            BindingMode.OneTime);
+
+        public string AdId
+        {
+            get => (string)GetValue(AdIdProperty);
+            set => SetValue(AdIdProperty, value);
+        }
     }
 }
