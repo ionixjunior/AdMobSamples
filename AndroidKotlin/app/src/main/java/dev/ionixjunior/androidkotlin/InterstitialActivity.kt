4fa553c94f2b7ce741867cf604cf06379b29ecab
Ione Souza Junior <junior@ionixjunior.com.br>
2021-04-22 20:23:26 -0300

    Adicionar activity para exibir os anúncinos do tipo interstitial

---
+++ AndroidKotlin/app/src/main/java/dev/ionixjunior/androidkotlin/InterstitialActivity.kt
@@ -0,0 +1,11 @@
+package dev.ionixjunior.androidkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InterstitialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_interstitial)
+    }
+}
