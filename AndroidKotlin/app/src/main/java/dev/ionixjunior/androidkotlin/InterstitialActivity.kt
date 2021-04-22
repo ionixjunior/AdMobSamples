ec3c96850814536c89991344c7dc58024826f457
Ione Souza Junior <junior@ionixjunior.com.br>
2021-04-22 20:45:12 -0300

    Exibir anúncio interstitial na tela

--- AndroidKotlin/app/src/main/java/dev/ionixjunior/androidkotlin/InterstitialActivity.kt
+++ AndroidKotlin/app/src/main/java/dev/ionixjunior/androidkotlin/InterstitialActivity.kt
@@ -2,10 +2,53 @@ package dev.ionixjunior.androidkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class InterstitialActivity : AppCompatActivity() {
+    private var mInterstitialAd: InterstitialAd? = null
+    private final var TAG = "InterstitialAD"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_interstitial)
+
+        var adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, adError?.message)
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d(TAG, "A propaganda foi carregada")
+                mInterstitialAd = interstitialAd
+
+                mInterstitialAd?.fullScreenContentCallback = object: FullScreenContentCallback() {
+                    override fun onAdDismissedFullScreenContent() {
+                        Log.d(TAG, "A propaganda foi descartada")
+                    }
+
+                    override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
+                        Log.d(TAG, "A propaganda falhou ao ser exibida")
+                    }
+
+                    override fun onAdShowedFullScreenContent() {
+                        Log.d(TAG, "A propaganda foi exibida em tela cheia")
+                        mInterstitialAd = null;
+                    }
+                }
+            }
+        })
+
+        var btMostrar = findViewById<Button>(R.id.btMostrar)
+        btMostrar.setOnClickListener { mInterstitialAd?.show(this) }
     }
 }
