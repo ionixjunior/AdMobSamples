93e941b5652843239d996cac21e60f806a866270
Ione Souza Junior <junior@ionixjunior.com.br>
2021-04-22 21:26:02 -0300

    Criar tela para exibir anúncio interstitial

---
+++ AdMobCross/AdMobCross/Views/InterstitialView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AdMobCross.Views
+{
+    public partial class InterstitialView : ContentPage
+    {
+        public InterstitialView()
+        {
+            InitializeComponent();
+        }
+    }
+}
