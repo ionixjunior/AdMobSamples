0d0207c780c7ad3b8e8d4584e48d5eecaf22a33c
Ione Souza Junior <junior@ionixjunior.com.br>
2021-04-29 20:23:55 -0300

    Remover activity lifecycle callbacks
    Isso funciona apenas na API 29 do Android e não vai servir para este projeto.

--- AdMobCross/AdMobCross.Android/Renderers/LifecycleLitePageRenderer.cs
+++ AdMobCross/AdMobCross.Android/Renderers/LifecycleLitePageRenderer.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using AdMobCross.Droid.Renderers;
+﻿using AdMobCross.Droid.Renderers;
 using AdMobCross.Interfaces;
-using Android.App;
 using Android.Content;
-using Android.OS;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
-using static Android.App.Application;
 
 [assembly: ExportRenderer(typeof(ContentPage), typeof(LifecycleLitePageRenderer))]
 namespace AdMobCross.Droid.Renderers
 {
     public class LifecycleLitePageRenderer : PageRenderer
     {
-        private IActivityLifecycleCallbacks _activityLifecycleCallbacks;
-        private Activity _activity;
-
         public LifecycleLitePageRenderer(Context context) : base(context)
         {
         }
@@ -26,59 +19,10 @@ namespace AdMobCross.Droid.Renderers
 
             if (e.OldElement is ILifecycleLite)
             {
-                _activity.UnregisterActivityLifecycleCallbacks(_activityLifecycleCallbacks);
             }
 
             if (e.NewElement is ILifecycleLite lifecycleLite)
             {
-                _activity = Context.GetActivity();
-                _activityLifecycleCallbacks = new CustomActivityLifecycleCallbacks(lifecycleLite);
-                _activity.RegisterActivityLifecycleCallbacks(_activityLifecycleCallbacks);
-            }
-        }
-
-        private class CustomActivityLifecycleCallbacks : Java.Lang.Object, IActivityLifecycleCallbacks
-        {
-            private readonly ILifecycleLite _lifecycleLite;
-
-            public CustomActivityLifecycleCallbacks(ILifecycleLite lifecycleLite)
-            {
-                _lifecycleLite = lifecycleLite;
-            }
-
-            public void OnActivityCreated(Android.App.Activity activity, Bundle savedInstanceState)
-            {
-                _lifecycleLite?.OnCreated();
-            }
-
-            public void OnActivityDestroyed(Android.App.Activity activity)
-            {
-                _lifecycleLite?.OnDestroyed();
-            }
-
-            public void OnActivityPaused(Android.App.Activity activity)
-            {
-                
-            }
-
-            public void OnActivityResumed(Android.App.Activity activity)
-            {
-                
-            }
-
-            public void OnActivitySaveInstanceState(Android.App.Activity activity, Bundle outState)
-            {
-                
-            }
-
-            public void OnActivityStarted(Android.App.Activity activity)
-            {
-                
-            }
-
-            public void OnActivityStopped(Android.App.Activity activity)
-            {
-                
             }
         }
     }
