eb40a76cb097f760602f3469223dc2f1c4447407
Ione Souza Junior <junior@ionixjunior.com.br>
2021-05-13 21:55:27 -0300

    Implementar anúncio premiado no iOS

--- AdMobCross/AdMobCross.iOS/Implementations/RewardediOS.cs
+++ AdMobCross/AdMobCross.iOS/Implementations/RewardediOS.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using AdMobCross.Interfaces;
 using AdMobCross.Models;
+using Foundation;
+using Google.MobileAds;
 
 namespace AdMobCross.iOS.Implementations
 {
-    public class RewardediOS : IRewarded
+    public class RewardediOS : NSObject, IRewarded
     {
         public event Action AdLoaded;
         public event Action AdFailed;
@@ -15,12 +17,52 @@ namespace AdMobCross.iOS.Implementations
 
         public void Load(string adId)
         {
-            
+            var request = Request.GetDefaultRequest();
+
+            RewardBasedVideoAd.SharedInstance.LoadRequest(request, adId);
+            RewardBasedVideoAd.SharedInstance.AdReceived += OnAdReceived;
+            RewardBasedVideoAd.SharedInstance.FailedToLoad += OnFailedToLoad;
+        }
+
+        private void OnAdReceived(object sender, EventArgs e)
+        {
+            AdLoaded?.Invoke();
+        }
+
+        private void OnFailedToLoad(object sender, RewardBasedVideoAdErrorEventArgs e)
+        {
+            AdFailed?.Invoke();
+        }
+
+        private void OnClosed(object sender, EventArgs e)
+        {
+            AdDismissedFullScreenContent?.Invoke();
+        }
+
+        private void OnOpened(object sender, EventArgs e)
+        {
+            AdShowedFullScreenContent?.Invoke();
+        }
+
+        private void OnUserRewarded(object sender, RewardBasedVideoAdRewardEventArgs e)
+        {
+            int.TryParse(e.Reward.Amount.ToString(), out int amount);
+
+            if (amount is 0)
+                return;
+
+            AdEarnReward?.Invoke(new RewardItem(amount, e.Reward.Type));
         }
 
         public void Show()
         {
-            
+            if (RewardBasedVideoAd.SharedInstance.IsReady)
+            {
+                RewardBasedVideoAd.SharedInstance.Closed += OnClosed;
+                RewardBasedVideoAd.SharedInstance.Opened += OnOpened;
+                RewardBasedVideoAd.SharedInstance.UserRewarded += OnUserRewarded;
+                RewardBasedVideoAd.SharedInstance.Present(Xamarin.Essentials.Platform.GetCurrentUIViewController());
+            }
         }
     }
 }
