2c32fa6d7043bd8b95d9c0594f23bbbfec55b098
Ione Souza Junior <junior@ionixjunior.com.br>
2021-08-26 20:39:05 -0300

    Carregar os anúncios na view controller

--- iOSSwift/iOSSwift/NativeViewController.swift
+++ iOSSwift/iOSSwift/NativeViewController.swift
@@ -1,7 +1,28 @@
 import UIKit
+import GoogleMobileAds
 
-class NativeViewController: UIViewController {
+class NativeViewController: UIViewController, GADNativeAdLoaderDelegate {
+    
+    var adLoader: GADAdLoader!
+    
     override func viewDidLoad() {
-        
+        let multipleAdsOptions = GADMultipleAdsAdLoaderOptions()
+        multipleAdsOptions.numberOfAds = 5
+
+        adLoader = GADAdLoader(
+            adUnitID: "ca-app-pub-3940256099942544/3986624511",
+            rootViewController: self,
+            adTypes: [.native],
+            options: [multipleAdsOptions])
+        adLoader.delegate = self
+        adLoader.load(GADRequest())
+    }
+    
+    func adLoader(_ adLoader: GADAdLoader, didReceive nativeAd: GADNativeAd) {
+        print("O anúncio nativo foi carregado")
+    }
+    
+    func adLoader(_ adLoader: GADAdLoader, didFailToReceiveAdWithError error: Error) {
+        print("O anúncio nativo não foi carregado")
     }
 }
