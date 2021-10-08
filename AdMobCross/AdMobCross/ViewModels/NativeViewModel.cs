38076b04f9c51e7a5131ede668bbce040c3e5e93
Ione Souza Junior <junior@ionixjunior.com.br>
2021-10-07 21:30:33 -0300

    Criar viewmodel para a tela das propagandas nativas

---
+++ AdMobCross/AdMobCross/ViewModels/NativeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using AdMobCross.Models;
+
+namespace AdMobCross.ViewModels
+{
+    public class NativeViewModel
+    {
+        public ObservableCollection<NativeItem> Items { get; }
+
+        public NativeViewModel()
+        {
+            Items = new ObservableCollection<NativeItem>(GetItems());
+        }
+
+        private List<NativeItem> GetItems()
+        {
+            var items = new List<NativeItem>();
+
+            for (var index = 1; index <= 50; index++)
+            {
+                items.Add(new NativeItem($"Título {index}", $"Descrição {index}"));
+            }
+
+            return items;
+        }
+    }
+}
