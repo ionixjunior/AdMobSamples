5481c70a4e20eae44b50a5284012344a4b226462
Ione Souza Junior <junior@ionixjunior.com.br>
2021-10-07 22:40:35 -0300

    Implementar lifecycle e carregar propaganda

--- AdMobCross/AdMobCross/Views/NativeView.xaml.cs
+++ AdMobCross/AdMobCross/Views/NativeView.xaml.cs
@@ -1,18 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using AdMobCross.Interfaces;
 using AdMobCross.ViewModels;
 using Xamarin.Forms;
 
 namespace AdMobCross.Views
 {
-    public partial class NativeView : ContentPage
+    public partial class NativeView : ContentPage, ILifecycleLite
     {
+        private readonly INative _native;
+
         public NativeView()
         {
             InitializeComponent();
-            var native = DependencyService.Resolve<INative>();
-            BindingContext = new NativeViewModel(native);
+            _native = DependencyService.Resolve<INative>();
+            BindingContext = new NativeViewModel(_native);
+        }
+
+        public void OnCreated()
+        {
+            Task.Run(() => _native.Load(Constants.AdNativeId, 5));
+            _native.AdLoaded += OnAdLoaded;
+        }
+
+        private void OnAdLoaded(Models.NativeAd nativeAd)
+        {
+            Console.WriteLine($"A propaganda chegou na view!!! {nativeAd.Title}");
+        }
+
+        public void OnDestroyed()
+        {
+            _native.AdLoaded -= OnAdLoaded;
         }
     }
 }
