4677800f5f450d2c237b7db143abd240ab87d449
Ione Souza Junior <junior@ionixjunior.com.br>
2021-10-07 21:43:13 -0300

    Criar classe para implementar INative no Android

---
+++ AdMobCross/AdMobCross.Android/Implementations/NativeAndroid.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using AdMobCross.Interfaces;
+
+namespace AdMobCross.Droid.Implementations
+{
+    public class NativeAndroid : INative
+    {
+        public void Load(string adId, int numberOfAds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
