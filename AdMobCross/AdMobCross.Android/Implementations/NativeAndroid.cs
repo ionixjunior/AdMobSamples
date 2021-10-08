9f398f0389915aa1f24294981b5d34363299a975
Ione Souza Junior <junior@ionixjunior.com.br>
2021-10-07 22:14:47 -0300

    Implementar carregamento da propaganda no Android

--- AdMobCross/AdMobCross.Android/Implementations/NativeAndroid.cs
+++ AdMobCross/AdMobCross.Android/Implementations/NativeAndroid.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using AdMobCross.Interfaces;
+using Android.Gms.Ads;
+using Android.Gms.Ads.Formats;
 
 namespace AdMobCross.Droid.Implementations
 {
@@ -7,7 +9,19 @@ namespace AdMobCross.Droid.Implementations
     {
         public void Load(string adId, int numberOfAds)
         {
-            throw new NotImplementedException();
+            var adLoader = new AdLoader.Builder(Xamarin.Essentials.Platform.AppContext, adId);
+            adLoader.ForUnifiedNativeAd(new UnifiedNativeAdLoadedListener());
+
+            var adRequest = new AdRequest.Builder().Build();
+            adLoader.Build().LoadAds(adRequest, numberOfAds);
+        }
+
+        private class UnifiedNativeAdLoadedListener : AdListener, UnifiedNativeAd.IOnUnifiedNativeAdLoadedListener
+        {
+            public void OnUnifiedNativeAdLoaded(UnifiedNativeAd ad)
+            {
+                Console.WriteLine($"Título: {ad.Headline} - Descrição: {ad.Body}");
+            }
         }
     }
 }
