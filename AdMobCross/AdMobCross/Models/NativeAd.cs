b1afae0ee0c735e2cea6bff3a4957f2104e2b26e
Ione Souza Junior <junior@ionixjunior.com.br>
2021-10-07 22:20:18 -0300

    Criar struct para representar os dados da propaganda nativa

---
+++ AdMobCross/AdMobCross/Models/NativeAd.cs
@@ -0,0 +1,16 @@
+﻿namespace AdMobCross.Models
+{
+    public struct NativeAd
+    {
+        public string Title { get; }
+        public string Description { get; }
+        public string Image { get; }
+
+        public NativeAd(string title, string description, string image)
+        {
+            Title = title;
+            Description = description;
+            Image = image;
+        }
+    }
+}
