ddcdc643ebe7d2fbd3be4f80c37d5d334b859729
Ione Souza Junior <junior@ionixjunior.com.br>
2021-10-14 20:49:25 -0300

    Carregar templates da propaganda e item no data template selector

--- AdMobCross/AdMobCross/DataTemplateSelectors/NativeViewDataTemplateSelector.cs
+++ AdMobCross/AdMobCross/DataTemplateSelectors/NativeViewDataTemplateSelector.cs
@@ -1,4 +1,6 @@
-﻿using AdMobCross.ViewCells;
+﻿using AdMobCross.Enums;
+using AdMobCross.Models;
+using AdMobCross.ViewCells;
 using Xamarin.Forms;
 
 namespace AdMobCross.DataTemplateSelectors
@@ -6,14 +8,25 @@ namespace AdMobCross.DataTemplateSelectors
     public class NativeViewDataTemplateSelector : DataTemplateSelector
     {
         private readonly DataTemplate _itemTemplate;
+        private readonly DataTemplate _adTemplate;
 
         public NativeViewDataTemplateSelector()
         {
             _itemTemplate = new DataTemplate(typeof(ItemCell));
+            _adTemplate = new DataTemplate(typeof(AdCell));
         }
 
-        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        protected override DataTemplate OnSelectTemplate(object obj, BindableObject container)
         {
+            if (obj is Item item)
+            {
+                return item.Type switch
+                {
+                    ItemType.Ad => _adTemplate,
+                    _ => _itemTemplate
+                };
+            }
+
             return _itemTemplate;
         }
     }
