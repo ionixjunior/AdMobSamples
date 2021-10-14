db2cf4622e6fa3a29c58ef3de1e317eb8efe37de
Ione Souza Junior <junior@ionixjunior.com.br>
2021-10-14 20:39:21 -0300

    Utilizar data template para exibir o item

---
+++ AdMobCross/AdMobCross/DataTemplateSelectors/NativeViewDataTemplateSelector.cs
@@ -0,0 +1,20 @@
+﻿using AdMobCross.ViewCells;
+using Xamarin.Forms;
+
+namespace AdMobCross.DataTemplateSelectors
+{
+    public class NativeViewDataTemplateSelector : DataTemplateSelector
+    {
+        private readonly DataTemplate _itemTemplate;
+
+        public NativeViewDataTemplateSelector()
+        {
+            _itemTemplate = new DataTemplate(typeof(ItemCell));
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            return _itemTemplate;
+        }
+    }
+}
