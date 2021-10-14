a0e0d51550aa7edd8d7c626929d022068db2e5c8
Ione Souza Junior <junior@ionixjunior.com.br>
2021-10-14 20:40:41 -0300

    Criar célula para a propaganda

---
+++ AdMobCross/AdMobCross/ViewCells/AdCell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace AdMobCross.ViewCells
+{
+    public partial class AdCell : ViewCell
+    {
+        public AdCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
