9821d1b2979c4e38645aa4d2a6362d9a935c1fa3
Ione Souza Junior <junior@ionixjunior.com.br>
2021-10-14 21:23:36 -0300

    Criar implementação do INative para iOS

---
+++ AdMobCross/AdMobCross.iOS/Implementations/NativeiOS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AdMobCross.Interfaces;
+using AdMobCross.Models;
+
+namespace AdMobCross.iOS.Implementations
+{
+    public class NativeiOS : INative
+    {
+        public event Action<NativeAd> AdLoaded;
+
+        public void Load(string adId, int numberOfAds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
